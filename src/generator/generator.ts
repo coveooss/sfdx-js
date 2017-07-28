09ab8b99d2b053aa3b2725f696ea401377119ced
Marc-Antoine Veilleux <maveilleux@coveo.com>
2017-07-28 12:16:48 -0400

    something seems to work with unit tests

--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -5,15 +5,19 @@ import { RootObject } from "./rootObject"
 export class Generator {
   constructor(private commandRunner: ICommandRunner) {}
 
-  public async generate() {
-    let result = await this.commandRunner.runCommand(
-      "force:doc:commands:display --json"
-    )
-    if (result.stderr !== undefined) {
-      throw new Error(result.stderr)
+  public async generate(json?: string) {
+    // If no JSON is passed, use SFDX to gather it.
+    if (json === undefined) {
+      let result = await this.commandRunner.runCommand(
+        "force:doc:commands:display --json"
+      )
+      if (result.stderr !== undefined) {
+        throw new Error(result.stderr)
+      }
+      json = result.stdout
     }
 
     let responseParser = new ResponseParser()
-    let rootObject = responseParser.parse<RootObject>(result.stdout)
+    let rootObject = responseParser.parse<RootObject>(json)
   }
 }
