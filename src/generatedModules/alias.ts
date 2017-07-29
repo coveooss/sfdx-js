f27ceabd5b6b096dbe2150166cbfbe2b1fbb1884
Marc-Antoine Veilleux <maveilleux@coveo.com>
2017-07-29 19:22:12 -0400

    Doc 2.0

--- src/generatedModules/alias.ts
+++ src/generatedModules/alias.ts
@@ -21,12 +21,12 @@ export class Alias {
   /**
     * list username aliases for sfdx
     *
-    * @param {Boolean} json Format output as JSON. 
-    * @param {string} loglevel The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log. 
+    * @param {Boolean} json Format output as JSON.
+    * @param {string} loglevel The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log.
     * @returns {(Object)}
     * @memberof Alias
     * @description Lists the aliases that sfdx can use for various commands and tasks.
-    * @example Example: 
+    * @example Example:
     *    $ sfdx force:alias:list
     * force:alias:list [--json] [--loglevel <string>]
     */
@@ -41,15 +41,15 @@ export class Alias {
   /**
     * set username aliases for sfdx
     *
-    * @param {Boolean} json Format output as JSON. 
-    * @param {string} loglevel The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log. 
+    * @param {Boolean} json Format output as JSON.
+    * @param {string} loglevel The logging level for this command invocation. Logs are stored in $HOME/.sfdx/sfdx.log.
     * @returns {(Object)}
     * @memberof Alias
     * @description Sets an alias that sfdx can use for various commands and tasks.
     * @example You can associate an alias with only one username at a time. If you’ve set an alias multiple times, the alias points to the most recent username.
-    * 
+    *
     * To delete an alias, run "sfdx force:alias:set" with no username.
-    * 
+    *
     * Examples:
     *    $ sfdx force:alias:set DefaultOrg=me@my.org
     *    $ sfdx force:alias:set DefaultOrg=me@my.org DevHubOrg=me@myhub.org
