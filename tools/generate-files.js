a33f81e07388a0313895244e7582889a31e9f800
Marc-Antoine Veilleux <maveilleux@coveo.com>
2018-09-11 13:01:43 -0400

    refactor: Refactor the code generator to an external npm project

--- tools/generate-files.js
+++ tools/generate-files.js
@@ -1,9 +1,13 @@
-var generator = require('../compiled_tools/generator')
+var generator = require('sfdx-generator')
 var path = require('path')
-console.log(generator)
-
 var root = path.resolve(__dirname, './..')
-var generator = new generator.Generator("sfdx", root)
+var generator = new generator.Generator({
+  SFDXPath: 'sfdx',
+  outputDirectory: path.resolve(root, 'src/generated'),
+  templateDirectory: path.resolve(root, './tools/templates'),
+  fileExtension: 'ts'
+});
+
 var commandFile = require('../commands.json')
 var stringFile = JSON.stringify(commandFile)
 generator.generate(stringFile).then(() => {
